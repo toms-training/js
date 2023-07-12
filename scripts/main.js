9cd18478f29efa6b019650793e7cec778b76aec0
tom4u <tom@ohms.pro>
2023-07-12 14:08:07 +0200

    feat(uebung02): task hinzufuegen

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,28 @@ const taskTemplate = {
     title: ''
 }
 
-function getTaskFromUser() {}
+function getTaskFromUser() {
+    const input = prompt('Titel der Aufgabe: ');
 
-function addTaskToDOM(task) {}
+    if (input === null || input.length === 0) return null;
+
+    let task = Object.create(taskTemplate);
+
+    task.id = tasks.length + 1;
+    task.title = input;
+
+    return task;
+}
+
+function addTaskToDOM(task) {
+    console.log(task);
+}
+
+let newTask = getTaskFromUser();
+
+while (newTask !== null) {
+    tasks.push(newTask);
+    newTask = getTaskFromUser();
+}
 
 tasks.forEach(addTaskToDOM);
